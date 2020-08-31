3564962d8fe221da7320be400621fc116600013a
coderJJ98 <jashjoshi.11098@gmail.com>
2020-08-31 12:16:03 +0530

    This file shows how classes can be created in php.

---
+++ Newclass.php
@@ -0,0 +1,10 @@
+<?php
+    class Newclass {
+        //properties and methods goes here.
+        public $info= "This is some info";
+    }
+//Initialization of class
+$object = new Newclass;
+var_dump($object)
+
+?>
