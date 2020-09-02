6939ec1a1cefcc19e80aed001e96fc74fbfafade
coderJJ98 <jashjoshi.11098@gmail.com>
2020-09-02 22:34:55 +0530

    This file show how to create function and methods

---
+++ Properties_methods.php
@@ -0,0 +1,13 @@
+<?php
+
+    class person{
+        //properties
+        public $name;
+        public $age;
+
+        //methods
+
+        public function setName($name){
+            $this ->name=$name;
+        }
+    }
